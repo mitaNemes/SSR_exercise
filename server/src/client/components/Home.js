0159fd38ff673f54fd344f83f8f1a00c24e27cbd
MihaiNemes <nemes.mihai.andrei@gmail.com>
2019-01-03 15:34:53 +0200

    init-commit

---
+++ server/src/client/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <div>BlaBla.........</div>
+            <button onClick={() => console.log('clicked')}>Click me</button>
+        </div>
+    );    
+}
+
+export default Home;
