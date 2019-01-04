8df20c16e098c7f1f4ef96d73990f979486d83ce
MihaiNemes <nemes.mihai.andrei@gmail.com>
2019-01-04 17:36:13 +0200

    client side redux

--- server/src/index.js
+++ server/src/index.js
@@ -1,11 +1,14 @@
+import 'babel-polyfill';
 import express from "express";
-import renderer from './client/helpers/renderer';
+import renderer from './helpers/renderer';
+import createStore from './helpers/createStore';
 
 const app = express();
 
 app.use(express.static('public'));
-app.get('/', (req, res) => {
-    res.send(renderer());
+app.get('*', (req, res) => {
+    const store = createStore();
+    res.send(renderer(req, store));
 });
 
 app.listen(3000, () => {
